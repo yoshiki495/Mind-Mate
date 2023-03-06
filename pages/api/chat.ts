579b85ab4a60c769c7ab645df281a8015d2de2ec
yoshiki495 <yoshiki-tanaka@cuebic.co.jp>
2023-03-07 00:29:03 +0900

    gpt-3.5-turboの導入

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -46,23 +46,11 @@ export default async function handler(req: NextRequest) {
 
   // const messages = req.body.messages
   const messagesPrompt = generatePromptFromMessages(body.messages)
-  const defaultPrompt = `I am Friendly AI Assistant. \n\nThis is the conversation between AI Bot and a news reporter.\n\n${botName}: ${firstMessge}\n${userName}: ${messagesPrompt}\n${botName}: `
-  const finalPrompt = process.env.AI_PROMPT
-    ? `${process.env.AI_PROMPT}${messagesPrompt}\n${botName}: `
-    : defaultPrompt
+  const promt = `I am Friendly AI Assistant. \n\nThis is the conversation between AI Bot and a news reporter.\n\n${botName}: ${firstMessge}\n${userName}: ${messagesPrompt}\n${botName}: `
 
   const payload = {
-    model: 'text-davinci-003',
-    prompt: finalPrompt,
-    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
-    max_tokens: process.env.AI_MAX_TOKENS
-      ? parseInt(process.env.AI_MAX_TOKENS)
-      : 200,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stop: [`${botName}:`, `${userName}:`],
-    user: body?.user,
+    model: 'gpt-3.5-turbo',
+    messages: [{ "role": "user", "content": promt }],
   }
 
   const requestHeaders: Record<string, string> = {
@@ -74,13 +62,15 @@ export default async function handler(req: NextRequest) {
     requestHeaders['OpenAI-Organization'] = process.env.OPENAI_API_ORG
   }
 
-  const response = await fetch('https://api.openai.com/v1/completions', {
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
     headers: requestHeaders,
     method: 'POST',
     body: JSON.stringify(payload),
   })
 
   const data = await response.json()
+
+  console.log(data.data)
 
   if (data.error) {
     console.error('OpenAI API error: ', data.error)
@@ -90,5 +80,5 @@ export default async function handler(req: NextRequest) {
   }
 
   // return response with 200 and stringify json text
-  return NextResponse.json({ text: data.choices[0].text })
+  return NextResponse.json({ text: data.choices[0].message.content })
 }
